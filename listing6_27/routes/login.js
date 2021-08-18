20dee309d45739f89042222ec4832cf3cb73f5fd
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-08-18 11:15:07 +0300

    feat: change listing6_27

--- listing6_27/routes/login.js
+++ listing6_27/routes/login.js
@@ -1,11 +1,12 @@
 const User = require('../models/user');
 
 exports.submit = (req, res, next) => {
-  const data = req.body.user;
+  const data = req.body.user; // {name: "ruslanmalovichko", pass: "1"}
   User.authenticate(data.name, data.pass, (err, user) => {
+    // user: User object: id: "1", name: "ruslanmalovichko", pass: "$2b$12$uiH3lxXixd2Ca4tsDhXPxOKk2enbEN.HYDpPt1AwTGfTgHO68L6dS", salt: "$2b$12$uiH3lxXixd2Ca4tsDhXPxO"
     if (err) return next(err);
     if (user) {
-      req.session.uid = user.id;
+      req.session.uid = user.id; // Add user id to session
       res.redirect('/');
     } else {
       res.error('Sorry! invalid credentials. ');
@@ -19,7 +20,7 @@ exports.form = (req, res) => {
 };
 
 exports.logout = (req, res) => {
-  req.session.destroy((err) => {
+  req.session.destroy((err) => { // Destroy session on logout
     if (err) throw err;
     res.redirect('/');
   })
