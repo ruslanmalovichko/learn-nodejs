f840c5c3f028be7042a1eb0fea1ed9465f56f24d
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-08-20 17:26:42 +0300

    feat: create listing7_5

---
+++ listing7_5/index.js
@@ -0,0 +1,27 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const http = require('http');
+const filename = './templates/students.ejs';
+const students = [
+  { name: 'Rick LaRue', age: 23 },
+  { name: 'Sarah Cathands', age: 25 },
+  { name: 'Bob Dobbs', age: 37 }
+];
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(filename, (err, data) => {
+      const template = data.toString(); // get template file data
+      const context = { students: students }; // Add object method students:
+      const output = ejs.render(template, context);
+      res.setHeader('Content-type', 'text/html');
+      res.end(output);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+server.listen(8000);
+
