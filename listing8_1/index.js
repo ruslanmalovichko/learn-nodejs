a6d01020b2d503b272b4e25f645bc0ddc38e871d
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-08-23 16:57:30 +0300

    feat: create listing8_1

---
+++ listing8_1/index.js
@@ -0,0 +1,9 @@
+const pg = require('pg');
+const db = new pg.Client({ database: 'articles' });
+
+db.connect((err, client) => {
+  if (err) throw err;
+  console.log('Connected to database', db.database);
+  db.end();
+});
+
