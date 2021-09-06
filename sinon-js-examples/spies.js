61e4a9f57cfa4e66b42853eff9863cdd0e9dc99d
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-09-06 18:09:36 +0300

    feat: create sinon-js-examples

---
+++ sinon-js-examples/spies.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Database = require('./db');
+const fs = require('fs');
+const database = new Database('./sample.json');
+
+const fsWriteFileSpy = sinon.spy(fs, 'writeFile');
+const saveDone = sinon.spy();
+
+database.insert('name', 'Charles Dickens');
+database.save(saveDone);
+
+sinon.assert.calledOnce(fsWriteFileSpy); // Check fsWriteFileSpy called once
+
+fs.writeFile.restore(); // Disable spy
+
